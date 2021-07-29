ed7571f4ec8e1c4411ac82e3d55881d3178d8e6c
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2021-07-29 19:34:48 +0300

    Fix url bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://dashboard.heroku.com/';
+export const baseUrl = 'https://dashboard.heroku.com';
 
 export const register = `${baseUrl}/users/register`;
 export const login = `${baseUrl}/users/login`;
